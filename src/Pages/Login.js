5990237c1d7fbbd2709f63abc643cc1d67ee8059
Naveen <sheoran19997@gmail.com>
2023-08-31 23:54:39 +0530

    add forget password

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -70,6 +70,38 @@ const Login = () => {
     }
   };
 
+  const forgotPasswordHandler = async () => {
+
+    let url;
+      url ="https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAnD99UOERjpJz0ImTtkH8j3TZD7WbvOnM";
+  
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          requestType:"PASSWORD_RESET", 
+          email: email.current.value, 
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (res.ok) {
+        
+        const data = await res.json();
+        alert('password reset email sent to : '+data.email);
+      } else {
+        const data = await res.json();
+        throw new Error(data.error.message);
+      }
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  
+
   return (
     <div className={classes.login}>
       <h1>{loginAccount ?  'Sign In ' : 'Sign Up'}</h1>
@@ -78,6 +110,7 @@ const Login = () => {
         <input id="email" type="email" ref={email} required />
         <label htmlFor="password">Password</label>
         <input id="password" type="password" ref={password} required minLength={7}/>
+        <p onClick={forgotPasswordHandler}>forgot password ?  get a reset link on email</p>
         <div>
           <button type="submit">
             {loginAccount ? "Login" : "Create Account"}
